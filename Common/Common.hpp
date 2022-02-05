5cd92e90a54afc4ec426a3f979c42d1676ca2470
Sirzhatina <mazurkevichsergey@gmail.com>
2022-02-05 20:35:47 +0300

    added msgs for client-server communication

--- Common/Common.hpp
+++ Common/Common.hpp
@@ -1,7 +1,14 @@
 #pragma once
 
 #include <cstdint>
+#include <string>
 
 typedef std::uint16_t Header;
 
 constexpr std::size_t headerSize = sizeof(Header);
+
+struct Message
+{
+    Header      size = 0;
+    std::string buffer;
+};
